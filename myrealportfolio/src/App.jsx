7404d9be49b6017c8e0e450e760eabf31dbfc71d
Teja Dasari <ichorlifephoenix@gmail.com>
2025-04-01 03:43:09 -0700

    Made API requests work.

--- myrealportfolio/src/App.jsx
+++ myrealportfolio/src/App.jsx
@@ -1,7 +1,31 @@
 import './App.css'
+import { use, useState } from 'react'
+import { useEffect } from 'react'
 import TrueMain from './components/trueMain.jsx'
 
 function App() {
+
+  console.log(import.meta.env.VITE_GITHUB_API_KEY);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://api.github.com/users/O-xix/repos', {
+        headers: {
+          'Authorization': `token ${import.meta.env.VITE_GITHUB_API_KEY}`
+        }
+      });
+      const data = await response.json();
+      data.forEach((repo) => {
+        console.log(repo.full_name);
+      }
+      );
+    }
+
+    fetchData();
+  }
+  , []);
+
+
 
   return (
     <>
@@ -9,7 +33,7 @@ function App() {
         <h1>My Real Portfolio</h1>
         <p>This is my real portfolio.</p>
       </div>
-      <TrueMain />
+      
     </>
   )
 }
